acd0636bf5b002c0ae4c5790cd0c47bb94ffcfd1
Кудрявцев Дмитрий Вальтерович <mitya-kudr@mail.ru>
2021-03-23 02:06:32 +0300

    add new form for set price

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,40 @@ import React, { useState, useEffect } from "react";
 import Web3 from 'web3';
 
 import {
-    validateUrl, validateTokenId, ERROR_TOKEN_ID_TEXT, ERROR_URL_TEXT, ERROR_CONTRACT_TEXT
+    validateUrl, validateTokenId, ERROR_TOKEN_ID_TEXT, ERROR_URL_TEXT, ERROR_CONTRACT_TEXT, ERROR_PRICE_TEXT
 } from "../utils/validator";
-import { CONTRACT_ADDRESS } from '../constants/contract';
-import CONTRACT_ABI from '../constants/mintNFT.json';
+import { CONTRACT_ADDRESS_MAIN, CONTRACT_ADDRESS_SELL } from '../constants/contract';
+import CONTRACT_MAIN_ABI from '../constants/mintNFT.json';
+import CONTRACT_SELL_ABI from '../constants/sellNFT_CryptoArteSales.json';
 
 import './App.css';
 
 const web3 = new Web3(Web3.givenProvider);
 
 export const App = () => {
-    const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+    const contractMain = new web3.eth.Contract(CONTRACT_MAIN_ABI, CONTRACT_ADDRESS_MAIN);
+    const contractSell = new web3.eth.Contract(CONTRACT_SELL_ABI, CONTRACT_ADDRESS_SELL);
     const [currentAccount, setCurrentAccount] = useState(undefined);
     const [url, setUrl] = useState('');
     const [tokenId, setTokenId] = useState(null);
     const [errors, setErrors] = useState([]);
     const [isDone, setIsDone] = useState(false);
 
+    const handleChangeTokenId = (e) => setTokenId(+e.target.value || null);
+    const handleChangeUrl = (e) => setUrl(e.target.value);
+
+    const [tokenIdPrice, setTokenIdPrice] = useState(null);
+    const [price, setPrice] = useState(null);
+
+    const handleChangePrice = (e) => setPrice(+e.target.value || null);
+    const handleChangeTokenIdPrice = (e) => setTokenIdPrice(+e.target.value || null);
+
     useEffect(() => {
         window.ethereum.enable();
         setCurrentAccount(web3.eth.accounts.currentProvider.selectedAddress.toLowerCase());
     }, []);
 
-    const handleChangeTokenId = (e) => setTokenId(+e.target.value || null);
-    const handleChangeUrl = (e) => setUrl(e.target.value);
-
-    const handleSubmit = async (e) => {
+    const handleSubmitMint = async (e) => {
         e.preventDefault();
         try {
             setErrors([]);
@@ -46,20 +54,40 @@ export const App = () => {
             }
 
             if (currentAccount && isValidTokenId && isValidUrl) {
-                await contract.methods.mint(currentAccount, tokenId, url).send({ from: currentAccount });
+                await contractMain.methods.mint(currentAccount, tokenId, url).send({ from: currentAccount });
                 setIsDone(true);
             }
         } catch (e) {
             setErrors([e.message]);
         }
-
     };
+
+    const handleSubmitSetPrice = async (e) => {
+        e.preventDefault();
+        try {
+            const isValidTokenIdPrice = validateTokenId(tokenIdPrice);
+            const isValidPrice = validateTokenId(price);
+
+            if (!isValidTokenIdPrice) {
+                setErrors((errors) => [...errors, ERROR_TOKEN_ID_TEXT]);
+            }
+            if (!isValidPrice) {
+                setErrors((errors) => [...errors, ERROR_PRICE_TEXT]);
+            }
+
+            if (currentAccount && isValidTokenIdPrice && isValidPrice) {
+                await contractSell.methods.mint(tokenIdPrice, price).send({ from: currentAccount });
+            }
+        } catch (e) {
+            setErrors([e.message]);
+        }
+    }
 
     return (
         <div className="App">
             <header className="App__header">NFT Marketplace</header>
             <div className="App__content">
-                <form className="App__form" onSubmit={handleSubmit}>
+                <form className="App__form" onSubmit={handleSubmitMint}>
                     <label className="App__form__label">
                         TokenId:
                         <input className="App__form__input" type="number" value={tokenId} onChange={handleChangeTokenId} />
@@ -69,6 +97,18 @@ export const App = () => {
                         <input className="App__form__input" type="text" value={url} onChange={handleChangeUrl} />
                     </label>
                     <input type="submit" className="App__form__button" disabled={!tokenId || !url} value="Mint" />
+                </form>
+
+                <form className="App__form" onSubmit={handleSubmitSetPrice}>
+                    <label className="App__form__label">
+                        TokenId:
+                        <input className="App__form__input" type="number" value={tokenIdPrice} onChange={handleChangeTokenIdPrice} />
+                    </label>
+                    <label className="App__form__label">
+                        Price:
+                        <input className="App__form__input" type="number" value={price} onChange={handleChangePrice} />
+                    </label>
+                    <input type="submit" className="App__form__button" disabled={!tokenIdPrice || !price} value="Set price" />
                 </form>
             </div>
             {
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -7,5 +7,6 @@ export const validateUrl = (url) => {
 }
 
 export const ERROR_TOKEN_ID_TEXT = 'Wrong tokenId: It should be greater than 0';
+export const ERROR_PRICE_TEXT = 'Wrong price: It should be greater than 0';
 export const ERROR_URL_TEXT = 'Wrong url: It should starts with https://';
 export const ERROR_CONTRACT_TEXT = 'Error with contract connection';
