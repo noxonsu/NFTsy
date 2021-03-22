38041c055ffb358e2c9f5bd39e3cf2591d4b3899
Кудрявцев Дмитрий Вальтерович <mitya-kudr@mail.ru>
2021-03-23 02:23:49 +0300

    add setApprovalForAll

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Web3 from 'web3';
 
 import {
-    validateUrl, validateTokenId, ERROR_TOKEN_ID_TEXT, ERROR_URL_TEXT, ERROR_CONTRACT_TEXT, ERROR_PRICE_TEXT
+    validateUrl, validateTokenId, validateAddress,
+    ERROR_TOKEN_ID_TEXT, ERROR_URL_TEXT, ERROR_CONTRACT_TEXT, ERROR_PRICE_TEXT, ERROR_ADDRESS_TEXT
 } from "../utils/validator";
 import { CONTRACT_ADDRESS_MAIN, CONTRACT_ADDRESS_SELL } from '../constants/contract';
 import CONTRACT_MAIN_ABI from '../constants/mintNFT.json';
@@ -13,8 +14,12 @@ import './App.css';
 const web3 = new Web3(Web3.givenProvider);
 
 export const App = () => {
+    useEffect(() => {
+        window.ethereum.enable();
+        setCurrentAccount(web3.eth.accounts.currentProvider.selectedAddress.toLowerCase());
+    }, []);
+
     const contractMain = new web3.eth.Contract(CONTRACT_MAIN_ABI, CONTRACT_ADDRESS_MAIN);
-    const contractSell = new web3.eth.Contract(CONTRACT_SELL_ABI, CONTRACT_ADDRESS_SELL);
     const [currentAccount, setCurrentAccount] = useState(undefined);
     const [url, setUrl] = useState('');
     const [tokenId, setTokenId] = useState(null);
@@ -23,17 +28,6 @@ export const App = () => {
 
     const handleChangeTokenId = (e) => setTokenId(+e.target.value || null);
     const handleChangeUrl = (e) => setUrl(e.target.value);
-
-    const [tokenIdPrice, setTokenIdPrice] = useState(null);
-    const [price, setPrice] = useState(null);
-
-    const handleChangePrice = (e) => setPrice(+e.target.value || null);
-    const handleChangeTokenIdPrice = (e) => setTokenIdPrice(+e.target.value || null);
-
-    useEffect(() => {
-        window.ethereum.enable();
-        setCurrentAccount(web3.eth.accounts.currentProvider.selectedAddress.toLowerCase());
-    }, []);
 
     const handleSubmitMint = async (e) => {
         e.preventDefault();
@@ -62,9 +56,18 @@ export const App = () => {
         }
     };
 
+    const contractSell = new web3.eth.Contract(CONTRACT_SELL_ABI, CONTRACT_ADDRESS_SELL);
+    const [tokenIdPrice, setTokenIdPrice] = useState(null);
+    const [price, setPrice] = useState(null);
+
+    const handleChangePrice = (e) => setPrice(+e.target.value || null);
+    const handleChangeTokenIdPrice = (e) => setTokenIdPrice(+e.target.value || null);
+
     const handleSubmitSetPrice = async (e) => {
         e.preventDefault();
         try {
+            setErrors([]);
+            setIsDone(false);
             const isValidTokenIdPrice = validateTokenId(tokenIdPrice);
             const isValidPrice = validateTokenId(price);
 
@@ -76,7 +79,30 @@ export const App = () => {
             }
 
             if (currentAccount && isValidTokenIdPrice && isValidPrice) {
-                await contractSell.methods.mint(tokenIdPrice, price).send({ from: currentAccount });
+                await contractSell.methods.setCurrentPrice(tokenIdPrice, price).send({ from: currentAccount });
+                setIsDone(true);
+            }
+        } catch (e) {
+            setErrors([e.message]);
+        }
+    }
+
+    const [addressToApprove, setAddressToApprove] = useState('');
+    const handleChangeAddressToApprove = (e) => setAddressToApprove(e.target.value);
+    const handleSubmitApprove = async (e) => {
+        e.preventDefault();
+        try {
+            setErrors([]);
+            setIsDone(false);
+            const isValidAddressToApprove = validateAddress(addressToApprove);
+
+            if (!isValidAddressToApprove) {
+                setErrors((errors) => [...errors, ERROR_ADDRESS_TEXT]);
+            }
+
+            if (currentAccount && isValidAddressToApprove) {
+                await contractMain.methods.setApprovalForAll(addressToApprove, true).send({ from: currentAccount });
+                setIsDone(true);
             }
         } catch (e) {
             setErrors([e.message]);
@@ -109,6 +135,14 @@ export const App = () => {
                         <input className="App__form__input" type="number" value={price} onChange={handleChangePrice} />
                     </label>
                     <input type="submit" className="App__form__button" disabled={!tokenIdPrice || !price} value="Set price" />
+                </form>
+
+                <form className="App__form" onSubmit={handleSubmitApprove}>
+                    <label className="App__form__label">
+                        Address:
+                        <input className="App__form__input" type="string" value={addressToApprove} onChange={handleChangeAddressToApprove} />
+                    </label>
+                    <input type="submit" className="App__form__button" disabled={!addressToApprove} value="Approve" />
                 </form>
             </div>
             {
