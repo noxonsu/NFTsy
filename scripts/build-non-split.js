4d0fa8b806332866e23dacd0bf49cf337758cb37
Кудрявцев Дмитрий Вальтерович <mitya-kudr@mail.ru>
2021-03-25 01:18:19 +0300

    remove minification + change path for main.js

--- scripts/build-non-split.js
+++ scripts/build-non-split.js
@@ -2,6 +2,7 @@ const rewire = require('rewire')
 const defaults = rewire('react-scripts/scripts/build.js') // If you ejected, use this instead: const defaults = rewire('./build.js')
 let config = defaults.__get__('config')
 
+config.optimization.minimize = false;
 config.optimization.splitChunks = {
     cacheGroups: {
         default: false
@@ -9,6 +10,6 @@ config.optimization.splitChunks = {
 }
 
 config.optimization.runtimeChunk = false
-config.output.filename = 'static/js/[name].js'
-config.plugins[5].options.filename = 'static/css/[name].css'
-config.plugins[5].options.moduleFilename = () => 'static/css/main.css'
+config.output.filename = '[name].js';
+config.plugins[5].options.filename = '[name].css';
+config.plugins[5].options.publicPath = '../';
