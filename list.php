075633d7da93b84051c241e8a70a613b7a445562
Noxon <2914674+noxonsu@users.noreply.github.com>
2021-04-07 01:00:03 +0300

    Create list.php

---
+++ list.php
@@ -0,0 +1,31 @@
+<?php 
+
+if ( isset( $_GET['token_id']{1} ) ) $nft_id = (int) $_GET['token_id'];
+$nft_current_page  = get_permalink( get_the_ID() );
+
+if (!isset($nft_id)) {
+	$args = array(
+        'post_type' => 'nft',
+        'posts_per_page' => -1
+    );
+	$query = new WP_Query($args);
+	if ($query->have_posts() ) : 
+		
+		while ( $query->have_posts() ) : $query->the_post();
+			$nft_link_id = add_query_arg( 'token_id', get_the_id() , $nft_current_page ) ;
+			$nft_link = get_the_title();
+			
+			echo '<a class="nftitem" href='.$nft_link_id.'><img src="'. get_the_title() . '"></a>';
+		endwhile;
+		
+		wp_reset_postdata();
+	endif;
+} else {
+	?>NFT ID: <?php echo $nft_id; ?>
+	<div id="root"></div>
+	<script>
+	window.init({ networkType: '<?php esc_html_e(get_option("nft_networkName"),"nft"); ?>', page: 'view', tokenId: <?php echo $nft_id; ?> })
+	</script><?
+}
+
+?>
