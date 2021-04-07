8e452f41ec39c412889f9e0bc5379cf16c9e23ef
Noxon <2914674+noxonsu@users.noreply.github.com>
2021-04-07 15:20:35 +0300

    Update list.php

--- list.php
+++ list.php
@@ -14,8 +14,15 @@ if (!isset($nft_id)) {
 		while ( $query->have_posts() ) : $query->the_post();
 			$nft_link_id = add_query_arg( 'token_id', get_the_id() , $nft_current_page ) ;
 			$nft_link = get_the_title();
-			
-			echo '<a class="nftitem" href='.$nft_link_id.'><img src="'. get_the_title() . '"></a>';
+			?>
+			<div class='nft_item'>
+			<a class="nftitem" href='<? echo $nft_link_id ?>'>
+				<img class='nft_img' src="<? echo get_the_content() ?>">
+			</a>
+			<div class='nft_title'><? echo get_the_title() ?></div> <div class='nft_price' rel="<? echo get_the_id(); ?>"> 1 ETH</div>
+			</div>
+			<?php
+			echo '';
 		endwhile;
 		
 		wp_reset_postdata();
